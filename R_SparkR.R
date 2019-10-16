5c0de26a36b6bf18b0fd21a76e7666e377c9b93c
Kurt Holst <kurtholst@hotmail.com>
2019-10-16 10:03:38 +0000

    Updated with latest code

---
+++ R_SparkR.R
@@ -0,0 +1,18 @@
+# Developing Machine Learning models with RStudio on Azure Databricks using Spark in memory training.
+
+# Install packages if needed.
+if (!require('mlbench')) install.packages('mlbench'); 
+
+
+# Load libraries
+library(mlbench)
+library(SparkR)
+
+# Load data
+data("PimaIndiansDiabetes")
+
+# Split data
+
+
+
+# Train model.
